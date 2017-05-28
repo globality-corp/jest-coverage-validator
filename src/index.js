f73ab8f2a7f8cf15c21f44f7d03cba24160d6e8b
Jason Morita <jason.morita@globality.com>
2017-05-27 17:09:03 -0700

    Reverse callsback

--- src/index.js
+++ src/index.js
@@ -15,24 +15,24 @@ const commands = {
 
 const HEAD_STRING = 'Jest: Coverage for';
 
-function getThresholds(config = {}) {
+export function getThresholds(config = {}) {
     return get(JSON.parse(config), 'globalConfig.coverageThreshold.global');
 }
 
-function splitResults(results = '') {
+export function splitResults(results = '') {
     return results.split(/\r?\n/);
 }
 
-function getLastLines(results = [], count = 0) {
+export function getLastLines(results = [], count = 0) {
     const resultsList = splitResults(results);
     return takeRight(compact(resultsList), count);
 }
 
-function matchLine(str = '', line = '') {
+export function matchLine(str = '', line = '') {
     return line.match(str);
 }
 
-function findFailures(thresholds = [], lines = []) {
+export function findFailures(thresholds = [], lines = []) {
     return thresholds.map((threshold) => {
         const matchString = `${HEAD_STRING} ${threshold}`;
         const appliedMatchLine = partial(matchLine, matchString);
@@ -41,7 +41,7 @@ function findFailures(thresholds = [], lines = []) {
     }).filter(failure => failure);
 }
 
-export default function validate(pass = Function, fail = Function) {
+export function validate(fail = Function, pass = Function) {
     const config = spawnSync(commands.base, commands.showConfig).stderr.toString();
     const results = spawnSync(commands.base, commands.coverage).stderr.toString();
     const thresholdList = Object.keys(getThresholds(config));
