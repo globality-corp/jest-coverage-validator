30a9c370bb3d0e8201370af99c6a61dcc5fb34af
Jason Morita <jason.morita@globality.com>
2017-08-31 10:28:42 -0700

    Fix cli args

--- dist/utils.js
+++ dist/utils.js
@@ -17,7 +17,7 @@ var _lodash = require('lodash');
 
 var commands = exports.commands = {
     base: 'jest',
-    coverage: ['--coverage --maxWorkers 4', '--json'],
+    coverage: ['--coverage', '--maxWorkers 4', '--json'],
     showConfig: ['--showConfig', '--json']
 };
 
